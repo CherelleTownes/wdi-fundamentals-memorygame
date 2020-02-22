2dfc8c99a005ee4ace730d1a0cb0fd64892a1701
moire <cherelletownestech@gmail.com>
2020-02-22 01:44:12 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,31 +1,21 @@
-// let cardOne = "queen";
-// let cardTwo = "queen";
-// let cardThree = "king";
-// let cardFour = "king";
-//
-// console.log("User flipped" + " " + cardOne);
-// console.log("User flipped" + " " + cardThree);
-
 const cards = ["queen", "queen", "king", "king"];
 cards;
 
-const cardsInPlay =[];
-cardsInPlay;
+const cardsInPlay = [];
 
-const cardOne = cards[0];
-cardOne;
+function checkForMatch() {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    console.log("You found a match!");
+  } else {
+    console.log("Sorry, try again.");
+  }
+};
 
-cardsInPlay.push("cardOne");
-console.log("User flipped queen")
+function flipCard(cardId) {
+  console.log("User flipped" + " " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+  checkForMatch();
+};
 
-const cardTwo = cards[2];
-cardTwo;
-
-cardsInPlay.push("cardTwo");
-console.log("User flipped king")
-
-if (cardsInPlay[0] === cardsInPlay[2]) {
-  alert("You found a match!");
-} else if (cardsInPlay[0] !== cardsInPlay[2]){
-  alert ("Sorry, try again.")
-}
+flipCard(0);
+flipCard(2);
